1fbd17fe06b0876d1f62ddef677a6ef0a31fafa8
Reshma122333 <61842389+Reshma122333@users.noreply.github.com>
2020-12-06 00:42:05 +0530

    turbidity

---
+++ turbidity/turbidity.ino
@@ -0,0 +1,18 @@
+void setup () {
+ Serial.begin(9600); // baud rate
+
+}
+
+
+void loop() {
+
+  int turbidity = analogRead(A0);
+  
+  float volt = turbidity*(5.0/1024.0)*3;
+  
+ Serial.println(volt);
+  
+  
+  delay(200);
+
+}
